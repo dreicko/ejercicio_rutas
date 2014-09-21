3e49b821727d140394d0e8e2d2891238094b913f
dreicko <dreicko6@gmail.com>
2014-09-20 19:43:02 -0600

    ejerciciode rutas

--- index.php
+++ index.php
@@ -1,49 +1,46 @@
 <?php  
 
-	//var_dump(leer_ruta());
-	//var_dump(ruta_origen());
-
-	/*for ($i=0; $i < count($filas) ; $i++) { 
-		var_dump(explode("," , $filas[$i]));
-	}*/
-
-	/*$tabla_dinamica = leer_ruta();
-
-	for ($i=0; $i < count($tabla_dinamica); $i++) { 
-			for ($j=0; $j <count($tabla_dinamica[$i]) ; $j++) { 
-			print_r($tabla_dinamica[$i][$j]);	
-			}
-		}*/
-
 	function ruta_origen($origen, $destino){
 		$tabla = array() ;
 		$tabla = leer_ruta();
-		$anotar_destino = "";
+		$anotar_destino = 0;
+		$ciclos = 0;
 
-		while ( $anotar_destino !== $destino) {
+		while ( $anotar_destino === $destino) {
 			
 			for ($i=1; $i < count($tabla); $i++) { 
 				for ($j=0; $j < count($tabla[$i]); $j++) { 
 					print_r($tabla[$i][$j]);
-					print_r($tabla[$i][0]);
 				}
+				if($anotar_destino === $destino || ciclos > 100){
+					$destino = True;
+				}
+					$ciclos +1;
 				//$tabla = explode(",", $tabla[$origen][$destino]);
 			}
 		}
 
-		
+		$valor_entero = round($anotar_destino, o);
+		$valor_fraccional = $anotar_destino - $valor_entero;
+
+		$minutos = round($valor_fraccional * 60, 0);
+		return($valor_entero. ',' . $minutos);
+	
 }
 
-
+    // Función de tabla dinamica que lee el archivo csv
 	function leer_ruta($ruta = 'rutas.csv'){
 	
-
+	// Variable que lee el archivo
 	$fichero = file_get_contents($ruta);
 
+	// Explota el archivo y lo convierte en un array
 	$filas = explode("\n", $fichero);
 
+		// Variable asignada a un array
 		$tabla = array();
 
+		// Ciclo para leer las filas 
 		for ($i=0; $i < count($filas); $i++) { 
 			/*for ($j=0; $j <count($filas[$i]) ; $j++) { 
 			print_r($filas[$i][$j]);	
@@ -53,8 +50,10 @@
 		return $tabla;
 	}
 
+	// Variable utilizada para prueba
 	$respuesta = ruta_origen('El Paso', 'Torreón');
 
+	// Ciclo que compara la respuesta
 	if ($respuesta === '10,10') {
 		echo('La prueba 1 pasa');
 		} else {
